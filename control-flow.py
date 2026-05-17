7d127adda9a6de59594c1785482c082f49abbd5e
Amirhossein Hajilari <56062640+haajilari@users.noreply.github.com>
2026-05-17 19:23:03 +0330

    control flow !

---
+++ control-flow.py
@@ -0,0 +1,16 @@
+bill_total = 210
+
+discount1 = 10
+discount2 = 20 
+
+if bill_total > 100 and bill_total < 200:
+    print("Bill is Greater than 100 and less than 200")
+    bill_total = bill_total - discount1
+elif bill_total > 200:
+    print("Bill is Greater than 200")
+    bill_total = bill_total - discount2
+else:
+    print("Bill is less than 100")
+
+
+print("total bill is: ", bill_total)
