5578d9b5ab00cdbe981eb78035ae6179433704e4
Amirhossein Hajilari <56062640+haajilari@users.noreply.github.com>
2026-05-18 08:45:49 +0330

    variable scopes

---
+++ variable-scopes.py
@@ -0,0 +1,20 @@
+
+# Global scope variable declared outside of all functions
+special = 5
+
+def get_total(a, b):
+    #enclosed scope variable declared inside a function
+    total = a + b
+    print(special)
+
+    def double_it():
+        #local variable
+        double = total * 2
+        print(special)
+
+    double_it()
+
+    return total
+
+# Built-in scope
+print(get_total(3, 4))
