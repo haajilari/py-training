ab9fb26ca11e0e902c336718d9b9cd6a8696479b
Amirhossein Hajilari <56062640+haajilari@users.noreply.github.com>
2026-05-24 19:39:37 +0330

    first # class

--- first-class.py
+++ first-class.py
@@ -1,11 +1,14 @@
-# Sample Solution code
-class MyFirstClass():
-    print("Who wrote this?")
+class MyFirstClass:
     index = "Author-Book"
+
+    def __init__(self):
+        print("Who wrote this?")  # Move the print statement into the constructor
 
     def hand_list(self, philosopher, book):
         print(MyFirstClass.index)
         print(philosopher + " wrote the book: " + book)
 
+# Instantiate the class
 whodunnit = MyFirstClass()
+# Call the method with parameters
 whodunnit.hand_list("Sun Tzu", "The Art of War")
