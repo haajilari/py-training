e7f5126eae3b9cfdfd4aabf81dda8be5d3048f6a
Amirhossein Hajilari <56062640+haajilari@users.noreply.github.com>
2026-05-24 12:30:02 +0330

    comprehenesions

--- comprehensions.py
+++ comprehensions.py
@@ -90,3 +90,18 @@ print(''.join(list(map(str, a))))
 z = ["alpha","bravo","charlie"]
 new_z = [i[0]*2 for i in z]
 print(new_z)
+
+def summation(n):
+   if n == 1:
+       return 0
+   return n + summation(n-1)
+
+a = summation(5)
+print(a)
+
+numbers = [15, 30, 47, 82, 95]
+def lesser(numbers):
+   return numbers < 50
+
+small = list(map(lesser, numbers))
+print(small)
