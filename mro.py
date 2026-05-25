e5761bc5586afa48257fba17d1bf3ef2605b24e5
Amirhossein Hajilari <56062640+haajilari@users.noreply.github.com>
2026-05-25 11:33:47 +0330

    mro

---
+++ mro.py
@@ -0,0 +1,31 @@
+class A:
+    def d(self):
+        return "Function inside A"
+
+class B:
+    def d(self):
+        return "Function inside B"
+
+
+class C:
+    def d(self):
+        return "Function inside C"
+c = C()
+print(c.d())
+
+class D(A, B):
+    def d(self):
+        return "Function inside D"
+
+
+class E(B, C):
+    def d(self):
+        return "Function inside E"
+
+
+class F(E,D,C):
+    pass
+
+f = F()
+print(f.d())
+print(F.mro())
