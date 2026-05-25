91025e54bdd9b9fd1e9e504b436b8051a3b679c7
Amirhossein Hajilari <56062640+haajilari@users.noreply.github.com>
2026-05-25 18:29:07 +0330

    check update

--- check_imports.py
+++ check_imports.py
@@ -3,8 +3,9 @@ import sys
 
 sys.path.append("/Users/lary/Desktop/Projects/py-training")
 
-from math import sqrt as sq
+from math import sqrt as sq , factorial as fact
 
 k = sq(1.21)
-
+f = fact(4)
 print(k)
+print(f)
